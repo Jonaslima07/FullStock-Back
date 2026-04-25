79ac51fafb1c8e55c61ab6a39589a663814da964
Jonaslima07 <jonaslima23122003@gmail.com>
2026-04-25 12:04:18 -0300

    ajustes

--- helpers/cors/__init__.py
+++ helpers/cors/__init__.py
@@ -8,6 +8,8 @@ def init_app(app):
             "http://127.0.0.1",
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "http://127.0.0.1:5174",
+            "http://localhost:5174",
             "http://localhost:3000",
             "http://127.0.0.1:3000",
             "http://127.0.0.1:5000"
